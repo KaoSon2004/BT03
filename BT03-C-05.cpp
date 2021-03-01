e15052411d1bfcf7c3f6fcd819757ed08b228e74
KaoSon2004 <78968595+KaoSon2004@users.noreply.github.com>
2021-03-01 20:32:10 +0700

    Add files via upload

---
+++ BT03-C-05.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int row, col;
+    cin >> row >> col;
+    int x = row;
+    int y = col;
+    int i, j, p = 0;
+    int k = 1;
+    int c[100][100];
+    while (k <= x * y) {
+        for (i = p; i < col; i++) {
+            c[p][i] = k++;
+        }
+        for (i = p + 1; i <row; i++) {
+            c[i][col - 1] = k++;
+        }
+        if (p != dong - 1) {
+            for (i = col - 2; i >= p; i--) {
+                c[row- 1][i] = k++;
+            }
+        }
+        if (p != col - 1) {
+            for (i = row- 2; i > p; i--) {
+                c[i][p] = k++;
+            }
+        }
+        p++; row--; col--;
+    }
+    for (i = 0; i < x; i++) {
+        for (j = 0; j < y; j++) {
+            cout << c[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
